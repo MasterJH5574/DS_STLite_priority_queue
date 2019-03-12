da183b9842d8c3de0d9999320b34eda4b1ab160d
MasterJH5574 <45167100+MasterJH5574@users.noreply.github.com>
2019-03-12 21:36:45 +0800

    final version

--- priority_queue.hpp
+++ priority_queue.hpp
@@ -7,66 +7,139 @@
 
 namespace sjtu {
 
-/**
- * a container like std::priority_queue which is a heap internal.
- * it should be based on the vector written by yourself.
- */
-template<typename T, class Compare = std::less<T>>
+template<typename T, class Compare = std::less<T> >
 class priority_queue {
+private:
+    struct node {
+        node *ls, *rs;
+        T *data;
+        int dis;
+
+        node() {
+            dis = -1;
+            data = nullptr;
+            ls = rs = nullptr;
+        }
+
+        node(const node &o) {
+            dis = o.dis;
+            data = new T(*o.data);
+            ls = rs = nullptr;
+        }
+
+        node(const T &x, const int &d) {
+            data = new T(x);
+            dis = d;
+            ls = rs = nullptr;
+        }
+    } *root;
+
+    size_t size1;
+
+    node* copy_construct(node* o) {
+        if (o == nullptr)
+            return nullptr;
+        node *p = new node(*o);
+        p->ls = copy_construct(o->ls);
+        p->rs = copy_construct(o->rs);
+        return p;
+    }
+
+    void deconstruct(node *o) {
+        if (o == nullptr)
+            return;
+        deconstruct(o->ls);
+        deconstruct(o->rs);
+        delete o;
+    }
+
+    void swap(node *&x, node *&y) {
+        node *p = x;
+        x = y, y = p;
+    }
+
+    int get_dis(node *p) const {
+        return p == nullptr ? -1 : p->dis;
+    }
+
+    node* merge_heap(node *x, node *y) {
+        if (x == nullptr)
+            return y;
+        if (y == nullptr)
+            return x;
+        Compare cmp;
+        if (cmp(*x->data, *y->data))
+            swap(x, y);
+        x->rs = merge_heap(x->rs, y);
+        if (get_dis(x->ls) < get_dis(x->rs))
+            swap(x->ls, x->rs);
+        x->dis = x->ls != nullptr ? x->ls->dis + 1 : 0;
+        return x;
+    }
+
 public:
-	/**
-	 * TODO constructors
-	 */
-	priority_queue() {}
-	priority_queue(const priority_queue &other) {}
-	/**
-	 * TODO deconstructor
-	 */
-	~priority_queue() {}
-	/**
-	 * TODO Assignment operator
-	 */
-	priority_queue &operator=(const priority_queue &other) {}
-	/**
-	 * get the top of the queue.
-	 * @return a reference of the top element.
-	 * throw container_is_empty if empty() returns true;
-	 */
-	const T & top() const {
-
+	priority_queue() {
+	    root = nullptr;
+	    size1 = 0;
 	}
-	/**
-	 * TODO
-	 * push new element to the priority queue.
-	 */
-	void push(const T &e) {
 
+	priority_queue(const priority_queue &o) {
+	    root = copy_construct(o.root);
+	    size1 = o.size1;
 	}
-	/**
-	 * TODO
-	 * delete the top element.
-	 * throw container_is_empty if empty() returns true;
-	 */
+
+	~priority_queue() {
+	    deconstruct(root);
+	}
+
+	priority_queue &operator=(const priority_queue &o) {
+        if (this == &o)
+            return *this;
+        deconstruct(root);
+        root = copy_construct(o.root);
+        size1 = o.size1;
+        return *this;
+	}
+
+	// throw container_is_empty if empty() returns true;
+	const T& top() const {
+	    if (empty())
+            throw container_is_empty();
+	    return *root->data;
+	}
+
+	void push(const T &x) {
+        node *p = new node(x, 0);
+        root = merge_heap(root, p);
+        size1++;
+	}
+
+	// throw container_is_empty if empty() returns true;
 	void pop() {
+        if (empty())
+            throw container_is_empty();
+        node *l = root->ls;
+        node *r = root->rs;
+        delete root;
 
+        root = merge_heap(l, r);
+        size1--;
 	}
-	/**
-	 * return the number of the elements.
-	 */
+
 	size_t size() const {
-
+        return size1;
 	}
-	/**
-	 * check if the container has at least an element.
-	 * @return true if it is empty, false if it has at least an element.
-	 */
+
 	bool empty() const {
-
+        return size1 == 0;
 	}
-	/**
-	 * return a merged priority_queue with at least O(logn) complexity.
-	 */
-	void merge(priority_queue &other) {
+
+	void merge(priority_queue &o) {
+	    //priority_queue o = O;
+	    root = merge_heap(root, o.root);
+	    size1 += o.size1;
+	    o.size1 = 0;
+	    o.root = nullptr;
 	}
 };
 
